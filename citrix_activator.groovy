5882aa08caad34d579ff6fd76fa449a53eb25111
Crow <e-amce@gft.com>
2020-11-02 17:31:57 +0000

    added citrix activator job

---
+++ citrix_activator.groovy
@@ -0,0 +1,26 @@
+pipelineJob("citrix-activator") {
+    description()
+    keepDependencies(false)
+    parameters {
+        stringParam("repourl", "", "")
+        stringParam("projectid", "", "")
+        stringParam("owner", "", "")
+        stringParam("configbucket", "", "")
+        stringParam("job_unique_id", "", "")
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url '$repourl'
+                        credentials("citrix")
+                    }
+                branch("MVP2")
+                }
+            }
+            scriptPath("Jenkinsfile")
+        }
+    }
+    disabled(false)
+}
