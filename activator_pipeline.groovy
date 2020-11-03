7b46ca713c5bb94a5f509e81e8d4dfaeda6f8532
zain-GFT <64900858+zain-GFT@users.noreply.github.com>
2020-11-03 13:03:20 +0000

    Update activator_pipeline.groovy

--- activator_pipeline.groovy
+++ activator_pipeline.groovy
@@ -7,6 +7,7 @@ pipelineJob("activator-pipeline") {
 		stringParam("projectid", "", "")
 		stringParam("activator_params", "size=100,region=europe-west2", "")
 		stringParam("job_unique_id", "", "")
+		stringParam("activator_name", "", "")
 	}
 	definition {
 		cpsScm {
