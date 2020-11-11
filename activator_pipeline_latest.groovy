b45f452af0f5f195aeb90addcb77e1073e3d5ef2
zain-GFT <64900858+zain-GFT@users.noreply.github.com>
2020-11-11 17:47:55 +0000

    Create activator_pipeline_latest.groovy

---
+++ activator_pipeline_latest.groovy
@@ -0,0 +1,43 @@
+pipelineJob("activator-pipeline") {
+	description()
+	keepDependencies(false)
+	authenticationToken('activatorbuild')
+	parameters {
+		stringParam("repourl", "", "")
+		stringParam("projectid", "", "")
+		stringParam("activator_params", "", "")
+		stringParam("job_unique_id", "", "")
+		stringParam("activator_name", "", "")
+	}
+  triggers {
+    genericTrigger {
+     genericVariables {
+      genericVariable {
+       key("activator_params")
+       value("\$.activator_params")
+       expressionType("JSONPath") //Optional, defaults to JSONPath
+       regexpFilter("") //Optional, defaults to empty string
+       defaultValue("") //Optional, defaults to empty string
+    }
+   }
+     token('activatorbuild')
+     printContributedVariables(true)
+     printPostContent(true)
+     silentResponse(false)
+     }
+    }
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						url '$repourl'
+						credentials("gituser")
+					}
+				}
+			}
+			scriptPath("Jenkinsfile")
+		}
+	}
+	disabled(false)
+}
